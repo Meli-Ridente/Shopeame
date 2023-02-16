8ebc88ee491e0b4b8ff08b7cd7ee5ecc994cdc7d
Melissa Ridente <melissaridente3e@gmail.com>
2023-02-16 19:12:33 +0100

    GetDataAPI

--- App/src/app/service.service.ts
+++ App/src/app/service.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { serviceInterface } from './modules/servicio.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ProductService {
-  productos: serviceInterface[];
-  constructor() {
-    this.productos = []
-    fetch('https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products')
-      .then(response => response.json())
-      .then(json => this.productos = json)
+  
+  productos: string = 'https://my-json-server.typicode.com/franlindebl/shopeame-api-v2/products';
+  constructor(private http: HttpClient) {}
+
+   getProduct(){
+    return this.http.get(`${this.productos}`)
    }
 }
